c538a39c9e55fa4e618e7dba43f6f4217d0a1c53
jaeyoon <dudwns1927@gmail.com>
2025-03-18 19:44:45 +0900

    (2025.3.18) 선택 정렬

---
+++ Class7th (Slieve of Eratostheres)/Source.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void sieve(int n)
+{
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    cout << "1부터 " << n << "까지의 소수: ";
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    while (1) {
+        int n;
+        cout << "n 값을 입력하세요: ";
+        cin >> n;
+
+        sieve(n);
+    }
+    return 0;
+
+}
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void sieve(int n)
+{
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    cout << "1부터 " << n << "까지의 소수: ";
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    while (1) {
+        int n;
+        cout << "n 값을 입력하세요: ";
+        cin >> n;
+
+        sieve(n);
+    }
+    return 0;
+
+}
+
+
+
--- program/program.cpp
+++ program/program.cpp
@@ -1,18 +1,54 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 
 
 
-int evclid(int x, int y)
-{
 
-    
+#pragma region 계수 정렬
+    // 데이터의 값을 비교하지 않고 각 원소에 데이터가
+    // 몇 개 있는지 갯수를 세어 저장한 다음 정렬하는 알고리즘 입니다.
+
+void countSort(vector<int>&arr) {
+    int maxVal = *max_element(arr.begin(), arr.end());
+
+
+    vector<int> count(maxVal + 1, 0);
+    for (int num : arr) {
+        count[num]++;
+    }
+
+
+    int index = 0;
+    for (int i = 0; i <= maxVal; i++) {
+        while (count[i] > 0) { 
+            arr[index++] = i; 
+            count[i]--;      
+        }
+    }
 }
 
 int main() {
+    vector<int> arr = { 1, 1, 3, 1, 2, 3, 4, 4, 2, 5 };
 
+    cout << "정렬 전: ";
+    for (int num : arr) cout << num << " ";
+    cout << endl;
 
+    countSort(arr);
+
+    cout << "정렬 후: ";
+    for (int num : arr) cout << num << " ";
+    cout << endl;
+
+    return 0;
 }
+
+
+
+
+#pragma endregion
+
 
 
 
