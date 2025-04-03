c5a78f314ec954309e8ed8019670157f6a867fb5
dudwns1927 <dudwns1927@gmail.com>
2025-04-03 19:44:27 +0900

    (2025.4.3) 집합

--- program/program.cpp
+++ program/program.cpp
@@ -1,51 +1,81 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>  // sort 사용을 위해 추가
 #define SIZE 8
 using namespace std;
-  
 
-class Kruskal
-{
-    class Edge
-    {
+class Kruskal {
+    class Edge {
     private:
         int vertexX;
         int vertexY;
         int weight;
     public:
         Edge(int vertexX, int vertexY, int weight)
-        {
-            this->vertexX = vertexX;
-            this->vertexY = vertexY;
-            this->weight = weight;
-        }
+            : vertexX(vertexX), vertexY(vertexY), weight(weight) {}
 
-        int getX() { return vertexX; }
-        int getY() { return vertexX; }
-        int getWeight() { return weight; }
+        int getX() const { return vertexX; }
+        int getY() const { return vertexY; }
+        int getWeight() const { return weight; }
     };
 
     int cost;
     int parent[SIZE];
     vector<Edge> nodeList;
 
-
 public:
-    Kruskal()
-    {
-        cost = 0;
-        
-        for (int i = 0; i < SIZE; i++)
-        {
+    Kruskal() : cost(0) {
+        for (int i = 0; i < SIZE; i++) {
             parent[i] = i;
         }
     }
 
-    void insert(int vertexX, int vertexY, int weight)
-    {
-        Edge edge(vertexX, vertexY, weight);
+    // Find the root of the set containing x (with path compression)
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
 
+    // Union the sets containing x and y
+    void unionSets(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+        }
+    }
+
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    void insert(int vertexX, int vertexY, int weight) {
+        Edge edge(vertexX, vertexY, weight);
         nodeList.push_back(edge);
+    }
+
+    void calculate() {
+
+        sort(nodeList.begin(), nodeList.end(), [](const Edge& a, const Edge& b) {
+            return a.getWeight() < b.getWeight();
+            });
+
+        for (int i = 0; i < nodeList.size(); i++) {
+            int x = nodeList[i].getX();
+            int y = nodeList[i].getY();
+            int weight = nodeList[i].getWeight();
+
+
+            if (!same(x, y)) {
+                cost += weight;
+                unionSets(x, y);
+                cout << "Selected Edge: (" << x << ", " << y << ") - Weight: " << weight << endl;
+                cout << "Current Cost: " << cost << endl << endl;
+            }
+        }
+
+        cout << "Total Minimum Cost: " << cost << endl;
     }
 };
 
@@ -53,20 +83,17 @@ int main() {
     Kruskal kruskal;
     kruskal.insert(1, 7, 10);
     kruskal.insert(4, 7, 14);
-
     kruskal.insert(1, 4, 30);
     kruskal.insert(4, 2, 25);
-
     kruskal.insert(1, 2, 64);
     kruskal.insert(1, 5, 19);
-    
     kruskal.insert(5, 7, 73);
     kruskal.insert(2, 5, 61);
-
     kruskal.insert(5, 3, 22);
     kruskal.insert(5, 6, 48);
     kruskal.insert(3, 6, 36);
-    
+
+    kruskal.calculate();
 
     return 0;
 }
